f00db20a06f041406ff064141e6963b7e8fb6128
pedromedeiros02 <pedro-medeiros@uergs.edu.br>
2025-05-24 15:47:21 -0300

    Adiciona strings em um buffer para printar na print_task()

--- main/virtual-cores-freertos.c
+++ main/virtual-cores-freertos.c
@@ -29,6 +29,37 @@ const char* getTaskStateName(int state) {
     }
 }
 
+#define BUFFER_SIZE 1024
+char string_buffer[BUFFER_SIZE];
+int buffer_index = 0;
+
+void addToStringBuffer(const char* str) {
+    int len = strlen(str);
+    if (buffer_index + len < BUFFER_SIZE - 1) {
+        strcpy(&string_buffer[buffer_index], str);
+        buffer_index += len;
+        string_buffer[buffer_index++] = '\n';  // Add newline after each entry
+        string_buffer[buffer_index] = '\0';    // Null-terminate
+    }
+    else {
+        // Buffer full; you can handle this as needed
+        
+    }
+}
+
+void printAndClearStringBuffer() {
+    if (buffer_index > 0) {
+        printf("Buffered Output:\n%s", string_buffer);
+        buffer_index = 0;
+        string_buffer[0] = '\0';
+    }
+    else {
+        printf("Buffer is empty.\n");
+    }
+}
+
+
+
 void initVirtualCores(){
     if(getSchedulingAlgorithm() == RMC){
         int virtualCoreZero = 0,virtualCoreOne = 0,virtualCoreTwo = 0,virtualCoreThree = 0;
@@ -87,16 +118,19 @@ void print_task(){
         printf("Idle 0 handle: %p , Idle 1 handle: %p\n", idleHandleArray[0], idleHandleArray[1]);
         printf("History of selected tasks:\n");
         printTaskIdsBuffer();
-
+        printAndClearStringBuffer();
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
 }
 
 void hello_task(void *pvParameter)
 {
+    struct Parameters* params = (struct Parameters*)pvParameter;
     while (1) {
     	// TaskHandle_t xHandle = xTaskGetCurrentTaskHandle();
         // printf("HELLO TASK\n");
+        //printf("Task ID: %d\n", params->period);
+        addToStringBuffer("Hello Task");
         vTaskDelay(1000 / portTICK_PERIOD_MS); // wait 1 second
     }
 }
